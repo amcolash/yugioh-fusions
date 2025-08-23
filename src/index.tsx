3eeea35fd51844511c62bcfcd192f766b2bc11a5
Andrew McOlash <amcolash@gmail.com>
2025-08-22 22:53:51 -0700

    begin react app, move images to public folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
-import {createRoot} from "react-dom/client";
-import App from "./App";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(<App/>)
+import "./index.css";
+import { App } from "./App";
+
+createRoot(document.getElementById("root")!).render(<App />);
