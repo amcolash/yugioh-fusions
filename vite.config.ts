f883d4032b96519fe5aa001e7194a0dfcf93659e
Andrew McOlash <amcolash@gmail.com>
2025-08-24 03:55:50 -0700

    fix vite build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  base: '/yugioh-fusions/',
   plugins: [tsconfigPaths(), tailwindcss()],
 });
