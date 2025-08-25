b851bce0a6d5a88d49ef6d9dcf0450e99fc59b40
Andrew McOlash <amcolash@gmail.com>
2025-08-25 13:36:43 -0700

    add recent modal, nicer button styling + smaller size

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,20 +3,40 @@ import { useState } from 'react';
 import { Background } from './Background';
 import { Fusions } from './Fusions';
 import { Hand } from './Hand';
+import { RecentModal } from './RecentModal';
 import { Search } from './Search';
+
+const recentCardsKey = 'recentCards';
 
 export function App() {
   const [hand, setHand] = useState<number[]>([]);
+  const [recentCards, setRecentCards] = useState<Record<string, number>>(
+    localStorage.getItem(recentCardsKey) ? JSON.parse(localStorage.getItem(recentCardsKey)!) : {}
+  );
+  const [dialogOpen, setDialogOpen] = useState(false);
 
   const addToHand = (id: number) => {
     setHand((prev) => [...prev, id]);
+
+    setRecentCards((prev) => {
+      const newCards = { ...prev, [id]: (prev[id] || 0) + 1 };
+      localStorage.setItem(recentCardsKey, JSON.stringify(newCards));
+      return newCards;
+    });
   };
 
   return (
     <div className="grid gap-8 max-w-screen">
       <Background type="fixed" />
       <h1 className="text-center">Yugi-Oh! Fusion Combinations</h1>
+
       <Search addToHand={addToHand} />
+
+      <RecentModal open={dialogOpen} setOpen={setDialogOpen} addToHand={addToHand} recentCards={recentCards} />
+      <button onClick={() => setDialogOpen(true)} className="mx-4">
+        Recent Cards
+      </button>
+
       <Hand hand={hand} setHand={setHand} />
       <Fusions hand={hand} setHand={setHand} />
     </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,33 +3,49 @@ import { friendlyName, stats } from 'utils/util';
 
 import { StatsOverlay } from './StatsOverlay';
 
-export function Card({ id, onClick, small = false }: { id: number; onClick?: () => void; small?: boolean }) {
+export function Card({
+  id,
+  onClick,
+  size = 'normal',
+}: {
+  id: number;
+  onClick?: () => void;
+  size?: 'x-small' | 'small' | 'normal';
+}) {
   const [showStats, setShowStats] = useState(false);
   const cardStats = stats[id];
 
+  let width = 'min-w-32';
+  if (size === 'small') width = 'min-w-28';
+  if (size === 'x-small') width = 'w-20';
+
   const inner = (
     <div
-      className="grid content-baseline justify-items-center gap-2 w-36 relative"
+      className={`grid content-baseline justify-items-center gap-2 relative ${size !== 'x-small' ? 'w-36' : ''}`}
       onMouseEnter={() => setShowStats(true)}
       onMouseLeave={() => setShowStats(false)}
     >
       <div className="relative h-fit">
         <img
-          className={`${small ? 'min-w-28' : 'min-w-32'} rounded border-2 border-amber-950`}
+          className={`${width} rounded border-2 border-amber-950`}
           src={`${import.meta.env.BASE_URL}/cropped/${id}.png`}
           alt=""
         />
-        <span className="absolute bottom-0 left-0 rounded-tr-sm text-sm rounded-bl-sm bg-gray-900 opacity-60 px-1">
-          #{id}
-        </span>
+        {size !== 'x-small' && (
+          <>
+            <span className="absolute bottom-0 left-0 rounded-tr-sm text-sm rounded-bl-sm bg-gray-900 opacity-60 px-1">
+              #{id}
+            </span>
 
-        <span className="absolute bottom-0 right-0 rounded-tl-sm text-sm text-right rounded-br-sm bg-gray-900 opacity-60 px-1">
-          {cardStats.attack}
-          <br />
-          {cardStats.defense}
-        </span>
+            <span className="absolute bottom-0 right-0 rounded-tl-sm text-sm text-right rounded-br-sm bg-gray-900 opacity-60 px-1">
+              {cardStats.attack}
+              <br />
+              {cardStats.defense}
+            </span>
+          </>
+        )}
       </div>
-      <span className="text-center wrap-anywhere">{friendlyName(id)}</span>
+      {size !== 'x-small' && <span className="text-center wrap-anywhere">{friendlyName(id)}</span>}
 
       {showStats && cardStats.cardType === 'Monster' && <StatsOverlay card={id} />}
     </div>
@@ -37,7 +53,7 @@ export function Card({ id, onClick, small = false }: { id: number; onClick?: () 
 
   if (onClick)
     return (
-      <button onClick={onClick} className={`${small ? 'min-w-28' : 'min-w-32'} transparent flex p-0`}>
+      <button onClick={onClick} className={`${width} transparent flex p-0`}>
         {inner}
       </button>
     );
