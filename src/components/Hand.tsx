45cbce41f1673ff5cd3e1213f23016871f569e78
Andrew McOlash <amcolash@gmail.com>
2025-08-27 14:01:10 -0700

    toggle cards between hand/field with click

--- src/components/Hand.tsx
+++ src/components/Hand.tsx
@@ -29,11 +29,13 @@ export function Hand({ hand, setHand }: { hand: SimpleCard[]; setHand: Dispatch<
           <CardSet
             cards={cardsInField}
             onRemove={(card) => setHand([...cardsInField.filter((c) => c.index !== card.index), ...cardsInHand])}
+            setHand={setHand}
           />
 
           <CardSet
             cards={cardsInHand}
             onRemove={(card) => setHand([...cardsInHand.filter((c) => c.index !== card.index), ...cardsInField])}
+            setHand={setHand}
           />
         </>
       )}
@@ -41,13 +43,33 @@ export function Hand({ hand, setHand }: { hand: SimpleCard[]; setHand: Dispatch<
   );
 }
 
-function CardSet({ cards, onRemove }: { cards: CardWithIndex[]; onRemove: (card: CardWithIndex) => void }) {
+function CardSet({
+  cards,
+  onRemove,
+  setHand,
+}: {
+  cards: CardWithIndex[];
+  onRemove: (card: CardWithIndex) => void;
+  setHand: Dispatch<SetStateAction<SimpleCard[]>>;
+}) {
   return (
     <ul className="flex gap-2 flex-wrap max-w-screen justify-center">
       {cards.map((card, index) => (
         <li key={index}>
           <div className="grid gap-2 h-full content-between justify-items-center">
-            <Card id={card.id} size="2x-small" />
+            <Card
+              id={card.id}
+              size="2x-small"
+              onClick={() => {
+                setHand((prev) => {
+                  const newCards = [...prev];
+                  const found = newCards.find((c) => c.id == card.id);
+                  found.location = found.location === 'field' ? 'hand' : 'field';
+
+                  return newCards;
+                });
+              }}
+            />
             <button className="danger !py-0 w-1/2" onClick={() => onRemove(card)}>
               X
             </button>
