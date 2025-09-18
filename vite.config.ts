04f0da06da0fa5fc145ba3a0c25686c25e137b9b
Andrew McOlash <amcolash@gmail.com>
2025-09-17 23:57:08 -0700

    upgrade deps + start work on pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,39 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { VitePWA } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   base: '/yugioh-fusions/',
-  plugins: [tsconfigPaths(), tailwindcss()],
+  plugins: [
+    tsconfigPaths(),
+    tailwindcss(),
+    ViteImageOptimizer({ png: { quality: 50 } }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: { globPatterns: ['**/*.{js,css,html,ico,png,svg}'] },
+
+      manifest: {
+        name: 'Yugi-Oh! Fusions',
+        short_name: 'Fusions',
+        start_url: '/yugioh-fusions/',
+        theme_color: '#0284c7',
+        background_color: '#161625',
+        icons: [
+          {
+            purpose: 'any',
+            sizes: '256x256',
+            src: 'icon.png',
+            type: 'image/png',
+          },
+        ],
+        orientation: 'portrait',
+        display: 'standalone',
+        lang: 'en-US',
+      },
+    }),
+  ],
   server: {
     host: '0.0.0.0',
   },
