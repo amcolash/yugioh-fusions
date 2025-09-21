45f5ffa248968799e1c72478c1299781282cc48c
Andrew McOlash <amcolash@gmail.com>
2025-09-21 13:58:15 -0600

    new icon, etc

--- vite.config.ts
+++ vite.config.ts
@@ -19,21 +19,28 @@ export default defineConfig({
     ViteImageOptimizer({ png: { quality: 50 } }),
     VitePWA({
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,map,woff2}'],
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,map,ttf}'],
         maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
       },
 
       manifest: {
         name: 'Yugi-Oh! Fusions',
         short_name: 'Fusions',
+        description: 'Yugi-Oh! Forbidden Memories fusions reference and deck builder',
         start_url: '/yugioh-fusions/',
         theme_color: '#0284c7',
         background_color: '#161625',
         icons: [
           {
-            purpose: 'any maskable',
+            purpose: 'any',
             sizes: '256x256',
             src: 'icon.png',
+            type: 'image/png',
+          },
+          {
+            purpose: 'maskable',
+            sizes: '192x192',
+            src: 'icon-maskable.png',
             type: 'image/png',
           },
         ],
